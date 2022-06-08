39747e565913544f96c7d761ae3369effaf071df
lagankukreja <46984654+lagankukreja@users.noreply.github.com>
2022-06-08 15:39:49 -0400

    Time: 560 ms (29.09%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-with cte as
-(select name, population, area from world where area>=3000000 or population>=25000000)
-select * from cte
+/* Write your PL/SQL query statement below */
+
+
+select name, population, area from world where area >= 3000000 or population >= 25000000;
